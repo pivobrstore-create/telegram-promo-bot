6e0ea2ed169964f47f25f5499bf846b4338df54e
pivobrstore-create <pivobrstore@gmail.com>
2025-11-25 09:33:07 -0300

    Refactor Amazon product data retrieval function

--- amazon.js
+++ amazon.js
@@ -1,34 +1,30 @@
 const axios = require('axios');
 
-function gerarLinkAfiliado(url, tag) {
-  return `${url}?tag=${tag}`;
-}
-
-async function getProductData(url, affiliateTag) {
+async function obterProdutoAmazon(url, afiliado) {
   try {
-    const response = await axios.get(url);
-    const html = response.data;
+    const { data } = await axios.get(url);
 
-    const title = html.match(/<title>(.*?)<\/title>/i)[1];
-    const price = (html.match(/R\$\s?([\d.,]+)/) || [null, "0"])[1];
-    const oldPrice = (html.match(/De:\s*R\$([\d.,]+)/) || [null, price])[1];
+    const nome = (data.match(/<title>(.*?)<\/title>/i) || ["", "Produto Amazon"])[1];
+    const precoAtual = (data.match(/priceblock_ourprice">R\$\s([\d.,]+)/i) || ["", "0"])[1];
+    const precoAntigo = (data.match(/priceblock_strikeprice">R\$\s([\d.,]+)/i) || ["", precoAtual])[1];
 
-    const discount = Math.round(
-      ((oldPrice - price) / oldPrice) * 100 || 0
-    );
+    const desconto = Math.round(((parseFloat(precoAntigo) - parseFloat(precoAtual)) / parseFloat(precoAntigo)) * 100 || 0);
 
     return {
-      title,
-      price,
-      oldPrice,
-      discount,
-      image: "https://via.placeholder.com/300",
-      tags: ["amazon", "oferta", "desconto"],
-      affiliateLink: gerarLinkAfiliado(url, affiliateTag)
+      nome,
+      precoAtual,
+      precoAntigo,
+      desconto,
+      parcelamento: "até 4x sem juros",
+      entrega: "frete grátis Prime",
+      imagem: "https://m.media-amazon.com/images/I/71ZpeFZ2bUL._AC_SL1500_.jpg",
+      tags: "#amazon #licor #bebidas #oferta",
+      linkAfiliado: `${url}?tag=${afiliado}`
     };
+
   } catch (e) {
     return null;
   }
 }
 
-module.exports = { getProductData };
+module.exports = { obterProdutoAmazon };
