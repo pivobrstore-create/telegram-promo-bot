0df365fc9065da73e0fb503ec24e8ee7ec075c1c
pivobrstore-create <pivobrstore@gmail.com>
2025-11-28 10:19:13 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const TelegramBot = require('node-telegram-bot-api');
 const { obterProdutoAmazon } = require('./amazonScraper');
 const { Client, LocalAuth, MessageMedia } = require('whatsapp-web.js');
